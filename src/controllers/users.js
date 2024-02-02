1543643b2d5026d609b29e0898d0d37206c16afa
Vishal <vishal64711@gmail.com>
2024-02-02 18:24:26 +0530

    controllers users complete

---
+++ src/controllers/users.js
@@ -0,0 +1,31 @@
+const { Users } = require('../db/models')
+const { genRandomUsername } = require('../utils/username')
+
+async function createAnonUser() {
+
+    const user = await Users.create({
+        username: genRandomUsername()
+    })
+
+    return user
+}
+
+async function getUserById(id) {
+    return await Users.findOne({ where: { id } })
+}
+
+async function getUserByUsername(username) {
+    return await Users.findOne({ where: { username } })
+}
+
+module.exports = {
+    createAnonUser,
+    getUserById,
+    getUserByUsername
+}
+
+async function yoi(){
+    console.log(await createAnonUser());
+}
+
+yoi()
